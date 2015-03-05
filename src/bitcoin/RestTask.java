78790a87b0174a3b67dcbdf3614b698fcaaea053
Manuel Bellet <manuel.bellet@techedgegroup.com>
2015-03-05 15:28:15 +0100

    1.0

--- src/bitcoin/RestTask.java
+++ src/bitcoin/RestTask.java
@@ -14,6 +14,19 @@ public class RestTask extends TimerTask {
 	private Type proxytype;
 	private String proxyaddress;
 	private int proxyport;
+	private String proxyuser;
+	private String proxypassword;
+	
+	public RestTask(String db_name, String db_user, String db_password, Type proxy_type, String proxy_address, int proxy_port, String proxy_user, String proxy_password) {
+		dbname = db_name;
+		dbuser = db_user;
+		dbpassword = db_password;
+		proxytype = proxy_type;
+		proxyaddress = proxy_address;
+		proxyport = proxy_port;
+		proxyuser = proxy_user;
+		proxypassword = proxy_password;
+	}
 	
 	public RestTask(String db_name, String db_user, String db_password, Type proxy_type, String proxy_address, int proxy_port) {
 		dbname = db_name;
@@ -22,45 +35,72 @@ public class RestTask extends TimerTask {
 		proxytype = proxy_type;
 		proxyaddress = proxy_address;
 		proxyport = proxy_port;
+	}	
+	
+	public RestTask(String db_name, String db_user, String db_password) {
+		dbname = db_name;
+		dbuser = db_user;
+		dbpassword = db_password;
 	}
 	
 	public void run() {
 
 		try { 
 
+			  RestJSONObj btc_ticker = null;
+			  RestJSONObj ltc_ticker = null; 
+			  RestJSONObj btc_mdepth = null; 
+			  RestJSONObj ltc_mdepth = null; 
+			  RestJSONArray btc_thistory = null; 
+			  RestJSONArray ltc_thistory = null;
+			  
 //	          db connection			
 			  jsontodb jsontodb = new jsontodb(Inet4Address.getLocalHost().getHostAddress(),3306,dbname,dbuser,dbpassword);
 			  
-//	          btc ticker table insert			
-			  RestJSONObj btc_ticker = new RestJSONObj("Ticker","BTC","https://www.okcoin.com/api/ticker.do?ok=1",proxytype,proxyaddress,proxyport);
+//            build json objects
+			  if (proxytype!=null && proxyuser!=null) {
+				  btc_ticker = new RestJSONObj("Ticker","BTC","https://www.okcoin.com/api/ticker.do?ok=1",proxytype,proxyaddress,proxyport,proxyuser,proxypassword);
+				  ltc_ticker = new RestJSONObj("Ticker","LTC","https://www.okcoin.com/api/ticker.do?symbol=ltc_usd&ok=1",proxytype,proxyaddress,proxyport,proxyuser,proxypassword);
+				  btc_mdepth = new RestJSONObj("MarketDepth","BTC","https://www.okcoin.com/api/depth.do?ok=1",proxytype,proxyaddress,proxyport,proxyuser,proxypassword);
+				  ltc_mdepth = new RestJSONObj("MarketDepth","LTC","https://www.okcoin.com/api/depth.do?symbol=ltc_usd&ok=1",proxytype,proxyaddress,proxyport,proxyuser,proxypassword);
+				  btc_thistory = new RestJSONArray("TradeHistory","BTC","https://www.okcoin.com/api/trades.do?since=5000&ok=1",proxytype,proxyaddress,proxyport,proxyuser,proxypassword);
+				  ltc_thistory = new RestJSONArray("TradeHistory","LTC","https://www.okcoin.com/api/trades.do?symbol=ltc_usd&since=5000&ok=1",proxytype,proxyaddress,proxyport,proxyuser,proxypassword);
+			  }
+			  else if (proxytype!=null && proxyuser==null) {
+				  btc_ticker = new RestJSONObj("Ticker","BTC","https://www.okcoin.com/api/ticker.do?ok=1",proxytype,proxyaddress,proxyport);
+				  ltc_ticker = new RestJSONObj("Ticker","LTC","https://www.okcoin.com/api/ticker.do?symbol=ltc_usd&ok=1",proxytype,proxyaddress,proxyport);
+				  btc_mdepth = new RestJSONObj("MarketDepth","BTC","https://www.okcoin.com/api/depth.do?ok=1",proxytype,proxyaddress,proxyport);
+				  ltc_mdepth = new RestJSONObj("MarketDepth","LTC","https://www.okcoin.com/api/depth.do?symbol=ltc_usd&ok=1",proxytype,proxyaddress,proxyport);
+				  btc_thistory = new RestJSONArray("TradeHistory","BTC","https://www.okcoin.com/api/trades.do?since=5000&ok=1",proxytype,proxyaddress,proxyport);
+				  ltc_thistory = new RestJSONArray("TradeHistory","LTC","https://www.okcoin.com/api/trades.do?symbol=ltc_usd&since=5000&ok=1",proxytype,proxyaddress,proxyport);				  
+			  }
+			  else {
+				  btc_ticker = new RestJSONObj("Ticker","BTC","https://www.okcoin.com/api/ticker.do?ok=1");
+				  ltc_ticker = new RestJSONObj("Ticker","LTC","https://www.okcoin.com/api/ticker.do?symbol=ltc_usd&ok=1");
+				  btc_mdepth = new RestJSONObj("MarketDepth","BTC","https://www.okcoin.com/api/depth.do?ok=1");
+				  ltc_mdepth = new RestJSONObj("MarketDepth","LTC","https://www.okcoin.com/api/depth.do?symbol=ltc_usd&ok=1");
+				  btc_thistory = new RestJSONArray("TradeHistory","BTC","https://www.okcoin.com/api/trades.do?since=5000&ok=1");
+				  ltc_thistory = new RestJSONArray("TradeHistory","LTC","https://www.okcoin.com/api/trades.do?symbol=ltc_usd&since=5000&ok=1");
+			  }
+			  
+//	          btc ticker table insert						  
 			  JSONObject json_btc_ticker = btc_ticker.get_jsonobj();		  
-			  jsontodb.jsonobj_insert(json_btc_ticker);
-			  
-//	          ltc ticker table insert		  
-			  RestJSONObj ltc_ticker = new RestJSONObj("Ticker","LTC","https://www.okcoin.com/api/ticker.do?symbol=ltc_usd&ok=1",proxytype,proxyaddress,proxyport);
+			  jsontodb.jsonobj_insert(json_btc_ticker);			  
+//	          ltc ticker table insert		  			  
 			  JSONObject json_ltc_ticker = ltc_ticker.get_jsonobj();
-			  jsontodb.jsonobj_insert(json_ltc_ticker);
-			  
-//	          btc marketdepth table insert			
-			  RestJSONObj btc_mdepth = new RestJSONObj("MarketDepth","BTC","https://www.okcoin.com/api/depth.do?ok=1",proxytype,proxyaddress,proxyport);
+			  jsontodb.jsonobj_insert(json_ltc_ticker);			  
+//	          btc marketdepth table insert						  
 			  JSONObject json_btc_mdepth = btc_mdepth.get_jsonobj();		  
-			  jsontodb.jsonobj_insert(json_btc_mdepth);
-			  
-//	          ltc marketdepth table insert			
-			  RestJSONObj ltc_mdepth = new RestJSONObj("MarketDepth","LTC","https://www.okcoin.com/api/depth.do?symbol=ltc_usd&ok=1",proxytype,proxyaddress,proxyport);
+			  jsontodb.jsonobj_insert(json_btc_mdepth);			  
+//	          ltc marketdepth table insert						  
 			  JSONObject json_ltc_mdepth = ltc_mdepth.get_jsonobj();		  
-			  jsontodb.jsonobj_insert(json_ltc_mdepth);		  
-			  
-//	          btc trade history
-			  RestJSONArray btc_thistory = new RestJSONArray("TradeHistory","BTC","https://www.okcoin.com/api/trades.do?since=5000&ok=1",proxytype,proxyaddress,proxyport);
+			  jsontodb.jsonobj_insert(json_ltc_mdepth);		  			  
+//	          btc trade history			  
 			  JSONArray json_btc_thistory = btc_thistory.get_jsonarray();
-			  jsontodb.jsonarray_insert(json_btc_thistory);
-			  	
-//	          ltc trade history
-			  RestJSONArray ltc_thistory = new RestJSONArray("TradeHistory","LTC","https://www.okcoin.com/api/trades.do?symbol=ltc_usd&since=5000&ok=1",proxytype,proxyaddress,proxyport);
+			  jsontodb.jsonarray_insert(json_btc_thistory);			  	
+//	          ltc trade history			  
 			  JSONArray json_ltc_thistory = ltc_thistory.get_jsonarray();
-			  jsontodb.jsonarray_insert(json_ltc_thistory);
-			  
+			  jsontodb.jsonarray_insert(json_ltc_thistory);			  
 			} 		  
 		    
 		    catch (Exception e) {	
--- src/bitcoin/db_fill.java
+++ src/bitcoin/db_fill.java
@@ -1,27 +1,92 @@
 package bitcoin;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.net.Proxy;
+import java.net.Proxy.Type;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class db_fill {    
     
 	public static void main(String[] args) {
-    
+		
+		String dbschema = null;
+		String dbuser = null;
+		String dbpassword = null;
+		Type proxytype = null;
+		String proxyaddress = null;
+		String proxyuser = null;
+		String proxypassword = null;
+		int proxyport = 0;
+		int period = 0;
+		TimerTask task = null;
+		
 		try { 
+//          read ini file
+			BufferedReader br = new BufferedReader(new FileReader("src/bitcoin/bitcoin.cfg"));
+		    String line = br.readLine();   
+//          set startup configuration		    
+	        while (line!=null) {
+	        	switch (line.substring(0,line.indexOf("="))) {
+	        		case "db_schema":
+	        			dbschema = line.substring(line.lastIndexOf("=")+1);
+	        			break;
+	        		case "db_user":
+	        			dbuser = line.substring(line.lastIndexOf("=")+1);
+	        			break;
+	        		case "db_password":
+	        			dbpassword = line.substring(line.lastIndexOf("=")+1);
+	        			break;	        			
+	        		case "proxy_type":
+	        			switch (line.substring(line.lastIndexOf("=")+1)) {
+	        				case "http": 
+	        					proxytype = Proxy.Type.HTTP; 
+	        					break;
+	        				case "socks": 
+	        					proxytype = Proxy.Type.SOCKS; 
+	        					break;
+	        			}
+	        			break;
+	        		case "proxy_address":
+	        			proxyaddress = line.substring(line.lastIndexOf("=")+1);
+	        			break;	 
+	        		case "proxy_port":
+	        			proxyport = (int) Integer.parseInt((String) line.substring(line.lastIndexOf("=")+1));
+	        			break;		
+	        		case "proxy_user":
+	        			proxyuser = line.substring(line.lastIndexOf("=")+1);
+	        			break;	
+	        		case "proxy_password":
+	        			proxypassword = line.substring(line.lastIndexOf("=")+1);
+	        			break;		        			
+	        		case "period":
+	        			period = (int) Integer.parseInt((String) line.substring(line.lastIndexOf("=")+1));
+	        			break;		        			
+	        	} 
+	        	line = br.readLine();
+	        }	
+	        br.close();
 //          build task			
-			TimerTask task = new RestTask("bitcoin","manwell77","1sjus7m3",Proxy.Type.HTTP,"192.168.201.3",3128);
+	        if (proxytype!=null && proxyuser!=null) {
+	        	task = new RestTask(dbschema,dbuser,dbpassword,proxytype,proxyaddress,proxyport,proxyuser,proxypassword);
+	        }
+	        else if (proxytype!=null && proxyuser==null) {
+	        	task = new RestTask(dbschema,dbuser,dbpassword,proxytype,proxyaddress,proxyport);	
+	        }
+	        else {
+	        	task = new RestTask(dbschema,dbuser,dbpassword);	
+	        }
 //          no period -> simply run			
-			if (args.length == 0) {	
+			if (period == 0) {	
 				task.run(); 
 			} 
 //	        period specified-> schedule				
 			else {     		    	 
 				Timer timer = new Timer();	    	    	
-				timer.schedule(task, 1000,(long) Long.parseLong((String) args[0]));
+				timer.schedule(task, 1000,period);
 			}
-		}	    	
-		
+		}	    			
 	    catch (Exception e) {	
 		  e.printStackTrace();		 
 		 } 			  
